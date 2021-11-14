2aab0db35a797ee4ff66375e0cd52b4ee2489a46
Alliix <delirioussoymilk@gmail.com>
2021-11-14 11:37:16 +0200

    read collected with labels

--- readCollected.js
+++ readCollected.js
@@ -1,8 +1,11 @@
 const fs = require("fs");
-const { brandConfig } = require("./brandConfig");
 
 const readTweetsLength = () => {
   const dir = "./peopleTweets";
+  let allNeutral = 0;
+  let allPositive = 0;
+  let allNegative = 0;
+  let allLength = 0;
   fs.readdir(dir, (err, files) => {
     if (err) {
       throw err;
@@ -11,13 +14,22 @@ const readTweetsLength = () => {
     files.forEach((file) => {
       let tweets = fs.readFileSync(`${dir}/${file}`, "utf-8");
       tweets = JSON.parse(tweets);
+      tweets = tweets.filter((tweet) => tweet.label != null);
+      allNeutral += tweets.filter((tweet) => tweet.label === 0).length;
+      allPositive += tweets.filter((tweet) => tweet.label === 1).length;
+      allNegative += tweets.filter((tweet) => tweet.label === 2).length;
       console.log(`${file}: ${tweets.length} tweets`);
+      allLength += tweets.length;
     });
+    console.log("All length: " + allLength);
+    console.log("All neutral length: " + allNeutral);
+    console.log("All positive length: " + allPositive);
+    console.log("All negative length: " + allNegative);
   });
 };
 
 const fixRimiTweets = () => {
-  const file = "./peopleTweets/rimi.json";
+  const file = "";
 
   let rimiTweets = fs.readFileSync(file, "utf-8");
   rimiTweets = JSON.parse(rimiTweets);
@@ -27,16 +39,8 @@ const fixRimiTweets = () => {
   const rimiPeopleTweets = rimiTweets.filter(
     (tweet) => tweet.userScreenName != "MansRimi"
   );
-  fs.writeFileSync(
-    "./brandTweets/rimi.json",
-    JSON.stringify(rimiBrandTweets),
-    "utf-8"
-  );
-  fs.writeFileSync(
-    "./peopleTweets/rimi.json",
-    JSON.stringify(rimiPeopleTweets),
-    "utf-8"
-  );
+  fs.writeFileSync("", JSON.stringify(rimiBrandTweets), "utf-8");
+  fs.writeFileSync("", JSON.stringify(rimiPeopleTweets), "utf-8");
 };
 
 readTweetsLength();
